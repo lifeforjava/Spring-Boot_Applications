58a23990f0630a1fb50459858da9c53b63535610
ankul <lifeforjava786@gmail.com>
2025-02-07 08:57:01 +0530

    Committed!!

---
+++ src/main/java/com/app/hcl/stock_exchange_service/controller/StockController.java
@@ -0,0 +1,27 @@
+package com.app.hcl.stock_exchange_service.controller;
+
+import com.app.hcl.stock_exchange_service.entity.Stock;
+import com.app.hcl.stock_exchange_service.service.StockService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/api/v1")
+public class StockController {
+
+    @Autowired
+    private StockService stockService;
+
+    @PostMapping("/stock")
+    public Stock createStock(@RequestBody Stock stock) {
+        return stockService.createStock(stock);
+    }
+
+   //http://localhost:8080/api/v1/stock?stockId=9&newPrice=1100
+    @PutMapping("/stock")
+    public Stock updateStockPrice(@RequestParam Long stockId, @RequestParam BigDecimal newPrice) {
+        return stockService.updateStockPrice(stockId, newPrice);
+    }
+}
